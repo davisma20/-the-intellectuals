f76a6a7608ed713941fd4a70781fdbaf38e80c1a
EC2 Default User <ec2-user@ip-172-31-32-88.us-east-2.compute.internal>
2022-05-23 05:07:17 +0000

    deleted folders

--- src/main/java/com/rantapp/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rantapp;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-		Message findById(long Id);
-}
